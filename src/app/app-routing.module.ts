a2ab14708e253454b30d2ac62c454322bee39196
Daniel Gunnlaugsson <daniel.thor.gunnlaugsson@gmail.com>
2018-02-23 14:43:21 +0700

    CSS grid, containers, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'calculator',
+    component: CalculatorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+/*--- COMPONENTS ---*/
 
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
 import { HomeComponent } from './components/home/home.component';
 
 
+// Meeting place for everything in the app
+
 @NgModule({
   declarations: [
     AppComponent,
-    CalculatorComponent,
-    HomeComponent
+    HomeComponent,
+    CalculatorComponent
   ],
-  imports: [
+  imports: [ // Modules
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // Services
+  bootstrap: [AppComponent] // Main app component
 })
 export class AppModule { }
