8b7d01757097b450e06443a6e55eb50dd7cd4dca
Daniel Gunnlaugsson <daniel.thor.gunnlaugsson@gmail.com>
2018-03-21 21:01:31 +0100

    SHA256 hash generator

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import * as crypto from 'crypto-js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  hashInput: string = '';
+  hashOutput: string = '';
+
+  constructor() {
+    console.log("constructor ran");
+  }
 
   ngOnInit() {
+    console.log("ngOnInit ran");
+    console.log(crypto.SHA256("MSG"));
+  }
+
+  hashString(s) {
+    this.hashOutput = crypto.SHA256(this.hashInput);
   }
 
 }
